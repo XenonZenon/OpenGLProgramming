23123c1c6a727417f26a9054b569a9d4c0dc86bb
Jayson B. Abad <88236674+XenonZenon@users.noreply.github.com>
2022-04-07 03:04:18 +0800

    Update vertex_shader.glsl

--- OpenGLProgrammingPractice/source/shaders/vertex_shader.glsl
+++ OpenGLProgrammingPractice/source/shaders/vertex_shader.glsl
@@ -1,5 +1,5 @@
 #version 400
-in vec3 vertex_point;
+layout (position = 0) in vec3 position;
 void main(){
-  gl_Position = vec4(vertex_point, 1.0);
+  gl_Position = vec4(position.x, position.y, position.z, 1.0);
 };
